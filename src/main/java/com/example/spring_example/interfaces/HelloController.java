241d95d5f08316a4229e119355eaa35851fbe6d6
hyun6ik <fkanwm@naver.com>
2022-01-28 16:52:08 +0900

    Initial commit

---
+++ src/main/java/com/example/spring_example/interfaces/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.spring_example.interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/ping")
+    public String hello() {
+        return "pong";
+    }
+}
