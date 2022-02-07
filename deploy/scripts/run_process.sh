14e67ac244b7705bd4d7d58d8c2b1bd15e0a50fc
hyun6ik <fkanwm@naver.com>
2022-02-07 19:08:58 +0900

    Fix #15 Jenkins - scripts/run_process.sh 변경

--- deploy/scripts/run_process.sh
+++ deploy/scripts/run_process.sh
@@ -7,9 +7,9 @@ ECR_DOCKER_IMAGE_NAME="spring-example"
 ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/${ECR_DOCKER_IMAGE_NAME}"
 ECR_DOCKER_TAG="1"
 
-aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${ECR_REPOSITORY}
+aws ecr get-login-password --region ${REGION} \
+  | docker login --username AWS --password-stdin ${ECR_REPOSITORY};
 
-export IMAGE=${ECR_DOCKER_IMAGE}
-export TAG=${ECR_DOCKER_TAG}
-
+export IMAGE=${ECR_DOCKER_IMAGE};
+export TAG=${ECR_DOCKER_TAG};
 docker-compose -f /home/ec2-user/docker-compose.yml up -d;
